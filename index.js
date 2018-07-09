9e7228a9afe879517852906e5e430cf7c3e5f0d0
rk-codes <ping.you.back@gmail.com>
2018-07-09 00:26:07 -0700

    modify scroll

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-$('.about-link').click(function() {
-    $('html, body').animate({
-      scrollTop: $('.about-container').offset().top
-    }, 800);
-  });
-  
-  $('.projects-link').click(function() {
-    $('html, body').animate({
-      scrollTop: $('.projects-container').offset().top
-    }, 800);
-  });
-  
-  $('.contact-link').click(function() {
-    $('html, body').animate({
-      scrollTop: $('.footer-container').offset().top
-    }, 800);
-  });
+ 
+$('.scroll').click(function(e) {
+  e.preventDefault();
+  $('body,html').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 1000 );
+});
